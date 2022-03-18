36165a1d9cf5760fd41afd6968248bc27ee2c6e0
arielsardinha <odararepresentacoes@outlook.com.br>
2022-03-18 19:01:32 -0300

    fix/desfazendo criando remedioService

--- lib/models/remedio_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:life_pet/models/pet_model.dart';
-
-class Remedio {
-  String nome, data;
-  int? id;
-  Pet pet;
-
-  Remedio({required this.nome, required this.data, required this.pet, this.id});
-}
