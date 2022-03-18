43e53a6269e677c07af60102869c4e956a252242
arielsardinha <odararepresentacoes@outlook.com.br>
2022-03-18 13:16:03 -0300

    adicionando camada de service

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,42 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:life_pet/models/pet_model.dart';
+import 'package:life_pet/services/pet_service.dart';
 
 class HomeController {
-  var pets = <Pet>[
-    Pet(
-      nome: "Toto",
-      descricao: "Um cachorro esperto",
-      cor: "Preto",
-      bio: "Um totó bem esperto",
-      imageUrl: "assets/images/toto.png",
-      idade: 2,
-      sexo: "Macho",
-      peso: 3.2,
-      id: 1,
-    ),
-    Pet(
-      nome: "Arnaldo",
-      descricao: "Pincher elétrico",
-      cor: "Preto",
-      bio: "Sou um pincher elétrico",
-      imageUrl: "assets/images/arnaldo.png",
-      idade: 3,
-      sexo: "Macho",
-      peso: 5.4,
-      id: 2,
-    ),
-    Pet(
-      nome: "Paulinho",
-      descricao: "Pincher elétrico",
-      cor: "Preto",
-      bio: "Sou um pincher elétrico",
-      imageUrl: "assets/images/arnaldo.png",
-      idade: 1,
-      sexo: "Macho",
-      peso: 12.3,
-      id: 3,
-    ),
-  ];
+  final _petService = PetService();
+  HomeController() {
+    pets = _petService.getAllPets();
+  }
+  var pets = <Pet>[];
 
   Future<void> acessPerfil(
       {required int index, required BuildContext context}) async {
---
+++ lib/services/pet_service.dart
@@ -0,0 +1,32 @@
+import 'package:life_pet/models/pet_model.dart';
+
+class PetService {
+  final _listaPet = <Pet>[
+    Pet(
+      nome: "Toto",
+      descricao: "Um cachorro esperto",
+      cor: "Preto",
+      bio: "Um totó bem esperto",
+      imageUrl: "assets/images/toto.png",
+      idade: 2,
+      sexo: "Macho",
+      peso: 3.2,
+      id: 1,
+    ),
+    Pet(
+      nome: "Arnaldo",
+      descricao: "Pincher elétrico",
+      cor: "Preto",
+      bio: "Sou um pincher elétrico",
+      imageUrl: "assets/images/arnaldo.png",
+      idade: 3,
+      sexo: "Macho",
+      peso: 5.4,
+      id: 2,
+    ),
+  ];
+
+  List<Pet> getAllPets() {
+    return _listaPet;
+  }
+}
