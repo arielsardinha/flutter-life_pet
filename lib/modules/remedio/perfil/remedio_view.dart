db0ba31e7cfd1dd1526235a7922133a3f6a05dbf
arielsardinha <odararepresentacoes@outlook.com.br>
2022-03-18 01:29:57 -0300

    criando cards de remédios

--- lib/modules/remedio/perfil/remedio_view.dart
+++ lib/modules/remedio/perfil/remedio_view.dart
@@ -10,53 +10,80 @@ class RemedioView extends StatelessWidget {
   Widget build(BuildContext context) {
     final pet = ModalRoute.of(context)!.settings.arguments as Pet;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 350,
-                  child: Image.asset(
-                    pet.imageUrl,
-                    fit: BoxFit.cover,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 350,
+                child: Image.asset(
+                  pet.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40, left: 10),
+                child: CircleAvatar(
+                  backgroundColor: Colors.redAccent,
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 40, left: 10),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.redAccent,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+            child: Row(
+              children: const [
+                Text(
+                  'Remédio',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
                 )
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              child: Row(
-                children: const [
-                  Text(
-                    'Remédio',
-                    style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView(children: [
+              cardRemedio(titulo: pet.nome, descricao: pet.descricao),
+            ]),
+          ),
+        ],
       ),
     );
   }
+}
+
+Widget cardRemedio({required String titulo, required String descricao}) {
+  return Card(
+    elevation: 8.0,
+    margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+    child: ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 28, vertical: 10),
+      leading: Container(
+        padding: const EdgeInsets.only(right: 12),
+        decoration: const BoxDecoration(
+          border: Border(
+            right: BorderSide(width: 1.0, color: Colors.redAccent),
+          ),
+        ),
+        child: const Icon(Icons.healing, color: Colors.redAccent),
+      ),
+      title: Text(
+        titulo,
+        style: const TextStyle(fontWeight: FontWeight.w400),
+      ),
+      subtitle: Text(descricao),
+    ),
+  );
 }
